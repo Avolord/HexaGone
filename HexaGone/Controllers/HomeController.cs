582128df3c7864702824e92f6bb6084704f91cb7
AvoLord <36458478+Avolord@users.noreply.github.com>
2020-03-25 13:41:40 +0100

    removed the comments and changed the color back

--- HexaGone/Controllers/HomeController.cs
+++ HexaGone/Controllers/HomeController.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using HexaGone.Models;
-// Niklas hat nen kommentar geschrieben
-//Anton auch
-// Niklas hat nen Kommentar geschrieben
-//Felix hat nen Kommentar geschrieben und Niklas' Kommentar verbessert
-//Arvid hat den besten Kommentar geschrieben
 
 
 namespace HexaGone.Controllers
