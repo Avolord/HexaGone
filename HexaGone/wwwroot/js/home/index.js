59b9cd0a40bb30a78202a5e4273ef3ffe7baa159
AvoLord <36458478+Avolord@users.noreply.github.com>
2020-03-27 09:45:58 +0100

    added fading animations to the login buttons

--- HexaGone/wwwroot/js/home/index.js
+++ HexaGone/wwwroot/js/home/index.js
@@ -1,17 +1,32 @@
-﻿function checkScroll() {
-    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+﻿let main_login_button_y;
 
-    console.log($('.navbar').hasClass('scrolled'));
+function checkScroll() {
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
     if ($(window).scrollTop() > startY) {
         $('.navbar').addClass("scrolled");
     } else {
         $('.navbar').removeClass("scrolled");
     }
+
+    if ($(window).scrollTop() > main_login_button_y) {
+        $('#navbar_login_btn').clearQueue();
+        $('.login-btn').clearQueue();
+        $('#navbar_login_btn').fadeIn("slow");
+        $('.login-btn').fadeOut("slow");
+
+    } else {
+        $('#navbar_login_btn').clearQueue();
+        $('.login-btn').clearQueue();
+        $('#navbar_login_btn').fadeOut("slow");
+        $('.login-btn').fadeIn("slow");
+    }
 }
 
 
 $(function () {
+    main_login_button_y = $('.login-btn').position().top;
+
     if ($('.navbar').length > 0) {
         $(window).on("scroll load resize", function () {
             checkScroll();
