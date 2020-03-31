d903941f189803a7123e5583f4cb6a37de62bcd3
Zkailor <62372965+Zkailor@users.noreply.github.com>
2020-03-31 10:34:46 +0200

    Create Hexmap.cs

---
+++ HexaGone/Models/Hexmap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HexagonalGrid.Models
+{
+    public class Hexmap
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public float hexWidth { get; set; }
+        public float hexHeight { get; set; }
+
+        public float hexSideLength { get; set; }
+
+        public Hex[,] hexes { get; set; }
+    }
+}
