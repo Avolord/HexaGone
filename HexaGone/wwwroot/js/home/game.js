2dd4b26152c7415fb413baed4a27cf2a933d6fbe
Zkailor <62372965+Zkailor@users.noreply.github.com>
2020-04-01 13:44:10 +0200

    Update game.js

--- HexaGone/wwwroot/js/home/game.js
+++ HexaGone/wwwroot/js/home/game.js
@@ -1,102 +1,201 @@
 ﻿(function () {
-    game_variables();
-    var off_directions = [
-        [[+1, 0], [0, -1], [-1, -1],
-        [-1, 0], [-1, +1], [0, +1]],
-        [[+1, 0], [+1, -1], [0, -1],
-        [-1, 0], [0, +1], [+1, +1]],
-    ];
+
+    gameVariables();
 
     //===
     // Test
-    document.getElementById("test1").innerHTML = textures;
+    //document.getElementById("test1").innerHTML = "hello";
     //===
 
-    hexHeight = Math.sin(hexagonAngle) * sideLength;
-    hexRadius = Math.cos(hexagonAngle) * sideLength;
-    hexRectangleHeight = sideLength + 2 * hexHeight;
-    hexRectangleWidth = 2 * hexRadius;
+    if (hexmapStyle == "pointy") {
+        gameVariables_p();
 
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
 
-        ctx.fillStyle = "#000000";
-        ctx.strokeStyle = "#CCCCCC";
-        ctx.lineWidth = 3;
+            ctx.fillStyle = "#CCCCCC";
+            ctx.strokeStyle = "#000000";
+            ctx.lineWidth = 3;
 
-        drawBoard(ctx, boardWidth, boardHeight);
+            drawBoard_p(ctx, boardWidth, boardHeight);
 
-        canvas.addEventListener("mousemove", function (eventInfo) {
-            var x,
-                y,
-                hexX,
-                hexY,
+
+            canvas.addEventListener("mousemove", function (eventInfo) {
+                var x,
+                    y,
+                    hexX,
+                    hexY;
 
                 x = eventInfo.offsetX || eventInfo.layerX;
-            y = eventInfo.offsetY || eventInfo.layerY;
+                y = eventInfo.offsetY || eventInfo.layerY;
 
-            hexY = Math.floor(y / (hexHeight + sideLength));
-            hexX = Math.floor((x - (hexY % 2) * hexRadius) / hexRectangleWidth);
+                hexY = Math.floor(y / (hexHeight + sideLength));
+                hexX = Math.floor((x - (hexY % 2) * hexRadius) / hexRectangleWidth);
+
+                
+                //===
+                // Compare original Hexagon(hexX, hexY) with two neighbors.
+                // The Hexagon with the shortest distance to the cursor is the correct one.
+                var p = offToPixel_p(hexX, hexY);
+                var d = distance(p[0], p[1], x, y);
+
+                var n2 = offNeighbor_p(hexX, hexY, 2);
+                var p2 = offToPixel_p(n2[0], n2[1]);
+                var d2 = distance(p2[0], p2[1], x, y);
+
+                var n1 = offNeighbor_p(hexX, hexY, 1);
+                var p1 = offToPixel_p(n1[0], n1[1]);
+                var d1 = distance(p1[0], p1[1], x, y)
 
 
-            //===
-            // Compare original Hexagon(hexX, hexY) with two neighbors.
-            // The Hexagon with the shortest distance to the cursor is the correct one.
-
-            var p = off_to_pixel(hexX, hexY);
-            var d = distance(p[0], p[1], x, y);
-
-            var n2 = off_neighbor(hexX, hexY, 2);
-            var p2 = off_to_pixel(n2[0], n2[1]);
-            var d2 = distance(p2[0], p2[1], x, y);
-
-            var n1 = off_neighbor(hexX, hexY, 1);
-            var p1 = off_to_pixel(n1[0], n1[1]);
-            var d1 = distance(p1[0], p1[1], x, y)
-
-
-            if (d2 < d && d2 < d1) {
-                hexX = n2[0];
-                hexY = n2[1];
-            }
-            else if (d1 < d) {
-                hexX = n1[0];
-                hexY = n1[1];
-            }
-            //===
-
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            drawBoard(ctx, boardWidth, boardHeight);
-
-            // Check if the mouse's coords are on the board
-            if (hexX >= 0 && hexX < boardWidth) {
-                if (hexY >= 0 && hexY < boardHeight) {
-                    ctx.fillStyle = "#000000";
-                    drawHexagon(ctx, hexX, hexY, true);
+                if (d2 < d && d2 < d1) {
+                    hexX = n2[0];
+                    hexY = n2[1];
                 }
-            }
+                else if (d1 < d) {
+                    hexX = n1[0];
+                    hexY = n1[1];
+                }
+                //===
+
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                drawBoard_p(ctx, boardWidth, boardHeight);
+
+                // Check if the mouse's coords are on the board
+                if (hexX >= 0 && hexX < boardWidth) {
+                    if (hexY >= 0 && hexY < boardHeight) {
+                        ctx.fillStyle = "#ffffff";
+                        ctx.globalAlpha = 0.5;
+                        drawHexagon_p(ctx, hexX, hexY, true);
+                        ctx.globalAlpha = 1.0;
+                    }
+                }
 
 
-        });
+            });
+        }
+    }
+    else if (hexmapStyle == "flat") {
+        gameVariables_f();
+
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+
+            ctx.fillStyle = "#CCCCCC";
+            ctx.strokeStyle = "#000000";
+            ctx.lineWidth = 3;
+
+            drawBoard_f(ctx, boardWidth, boardHeight);
+
+
+            canvas.addEventListener("mousemove", function (eventInfo) {
+                var x,
+                    y,
+                    hexX,
+                    hexY;
+
+                x = eventInfo.offsetX || eventInfo.layerX;
+                y = eventInfo.offsetY || eventInfo.layerY;
+
+                hexX = Math.floor(x / (hexHeight + sideLength));
+                hexY = Math.floor((y - (hexX % 2) * hexRadius) / hexRectangleHeight);
+
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                drawBoard_f(ctx, boardWidth, boardHeight);
+
+                //===
+                // Compare original Hexagon(hexX, hexY) with its neighbors.
+                // The Hexagon with the shortest distance to the cursor is the correct one.
+                var p = offToPixel_f(hexX, hexY);
+                var d = distance(p[0], p[1], x, y);
+
+                var n3 = offNeighbor_f(hexX, hexY, 3);
+                var p3 = offToPixel_f(n3[0], n3[1]);
+                var d3 = distance(p3[0], p3[1], x, y);
+
+                var n4 = offNeighbor_f(hexX, hexY, 4);
+                var p4 = offToPixel_f(n4[0], n4[1]);
+                var d4 = distance(p4[0], p4[1], x, y)
+
+
+                document.getElementById("test1").innerHTML = x;
+                document.getElementById("test2").innerHTML = y;
+                document.getElementById("test3").innerHTML = "test";
+                document.getElementById("test4").innerHTML = "test";
+
+
+                if (d3 < d && d3 < d4) {
+                    hexX = n3[0];
+                    hexY = n3[1];
+                }
+                else if (d4 < d) {
+                    hexX = n4[0];
+                    hexY = n4[1];
+                }
+                //===
+
+                // Check if the mouse's coords are on the board
+                if (hexX >= 0 && hexX < boardWidth) {
+                    if (hexY >= 0 && hexY < boardHeight) {
+                        ctx.fillStyle = "#ffffff";
+                        ctx.globalAlpha = 0.5;
+                        drawHexagon_f(ctx, hexX, hexY, true);
+                        ctx.globalAlpha = 1.0;
+                    }
+                }
+            });
+        }
     }
 
-    function drawBoard(canvasContext, width, height) {
+    
+    
+    
+    
+
+
+    //=====
+    // Functions:
+    function distance(x1, y1, x2, y2) {
+        return Math.round(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)))
+    }
+    //=====
+
+
+    //=====
+    // Functions for pointy top hexagons:
+    function drawBoard_p(canvasContext, width, height) {
         var i,
             j;
 
         for (i = 0; i < width; ++i) {
             for (j = 0; j < height; ++j) {
-                drawHexagon(
-                    ctx,
-                    i,
-                    j,
-                    false
-                );
+                drawColoredHex_p(canvasContext, i, j, textures[i][j]);
             }
         }
     }
 
-    function drawHexagon(canvasContext, x, y, fill) {
+    function drawColoredHex_p(canvasContext, x, y, colorIndex) {
+        switch (colorIndex) {
+            case 0:
+                canvasContext.fillStyle = "#95e639";
+                break;
+            case 1:
+                canvasContext.fillStyle = "#42d4b9";
+                break;
+            case 2:
+                canvasContext.fillStyle = "#a342d4";
+                break;
+            case 3:
+                canvasContext.fillStyle = "#d49242";
+                break;
+            default:
+                canvasContext.fillStyle = "#bab9b8";
+                break
+        }
+        drawHexagon_p(canvasContext, x, y, true);
+    }
+    
+    function drawHexagon_p(canvasContext, x, y, fill) {
         var fill = fill || false;
 
         x = x * hexRectangleWidth + ((y % 2) * hexRadius);
@@ -118,20 +217,7 @@
         }
     }
 
-    function pixel_to_axial(x, y) {
-        // set pixel coordinates to the middle of the hexagon
-        //x = x + hexRadius;
-        //y = y + sideLength;
-
-        var q = (Math.sqrt(3) / 3 * x - 1 / 3 * y) / sideLength;
-        var r = (2. / 3 * y) / sideLength;
-
-        var axial = axial_round(q, r)
-
-        return [axial[0], axial[1]];
-    }
-
-    function axial_to_pixel(q, r) {
+    function axialToPixel_p(q, r) {
         var x = sideLength * (Math.sqrt(3) * q + Math.sqrt(3) / 2 * r);
         var y = sideLength * (3. / 2 * r);
 
@@ -142,92 +228,131 @@
         return [x, y];
     }
 
-    function off_to_pixel(x, y) {
-        var axial = off_to_axial(x, y);
-        var pixel = axial_to_pixel(axial[0], axial[1]);
+    function offToPixel_p(x, y) {
+        var axial = offToAxial_p(x, y);
+        var pixel = axialToPixel_p(axial[0], axial[1]);
         return [pixel[0], pixel[1]];
     }
 
-    function cube_round(x, y, z) {
-        var rx = Math.round(x);
-        var ry = Math.round(y);
-        var rz = Math.round(z);
-
-        var x_diff = Math.abs(rx - x);
-        var y_diff = Math.abs(ry - y);
-        var z_diff = Math.abs(rz - z);
-
-        if (x_diff > y_diff && x_diff > z_diff) {
-            rx = -ry - rz;
-        }
-        else if (y_diff > z_diff) {
-            ry = -rx - rz;
-        }
-        else {
-            rz = -rx - ry;
-        }
-
-        return [rx, ry, rz];
-    }
-
-    function axial_round(x, y) {
-
-        var cube = axial_to_cube(x, y);
-        var rCube = cube_round(cube[0], cube[1], cube[2]);
-        var axial = cube_to_axial(rCube[0], rCube[1], rCube[2])
-
-        return [axial[0], axial[1]];
-    }
-
-    function axial_to_cube(x, z) {
-
-        var y = -x - z;
-
-        return [x, y, z];
-    }
-
-    function cube_to_axial(x, y, z) {
+    function cubeToAxial_p(x, y, z) {
 
         return [x, z];
     }
 
-    function off_to_cube(col, row) {
+    function offToCube_p(col, row) {
         var x = col - (row - (row & 1)) / 2;
         var z = row;
         var y = -x - z;
         return [x, y, z];
     }
 
-    function cube_to_off(x, y, z) {
-        var col = x + (z - (z & 1)) / 2;
-        var row = z;
-        return [col, row];
-    }
-
-    function off_to_axial(row, col) {
-        var cube = off_to_cube(row, col);
-        var axial = cube_to_axial(cube[0], cube[1], cube[2])
+    function offToAxial_p(row, col) {
+        var cube = offToCube_p(row, col);
+        var axial = cubeToAxial_p(cube[0], cube[1], cube[2])
         return [axial[0], axial[1]];
     }
 
-    function axial_to_off(p, r) {
-        var cube = axial_to_cube(p, r);
-        var off = cube_to_off(cube[0], cube[1], cube[2]);
-        return [off[0], off[1]];
-    }
-
-    function distance(x1, y1, x2, y2) {
-        return Math.round(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)))
-    }
-
-    function axial_neighbor(p, r, direction) {
-        var dir = axial_directions[direction];
-        return [p + dir[0], r + dir[1]];
-    }
-
-    function off_neighbor(col, row, direction) {
+    function offNeighbor_p(col, row, direction) {
         var parity = row & 1;
-        var dir = off_directions[parity][direction];
+        var dir = offDirections[parity][direction];
         return [col + dir[0], row + dir[1]];
     }
+    //=====
+
+
+    //=====
+    // Functions for flat top hexagons:
+    function drawBoard_f(canvasContext, width, height) {
+        for (var i = 0; i < width; i++) {
+            for (var j = 0; j < height; j++) {
+                drawColoredHex_f(canvasContext, i, j, textures[i][j]);
+            }
+        }
+    }
+
+    function drawColoredHex_f(canvasContext, col, row, colorIndex) {
+        switch (colorIndex) {
+            case 0:
+                canvasContext.fillStyle = "#95e639";
+                break;
+            case 1:
+                canvasContext.fillStyle = "#42d4b9";
+                break;
+            case 2:
+                canvasContext.fillStyle = "#a342d4";
+                break;
+            case 3:
+                canvasContext.fillStyle = "#d49242";
+                break;
+            default:
+                canvasContext.fillStyle = "#bab9b8";
+                break
+        }
+
+        drawHexagon_f(canvasContext, col, row, true);
+    }
+
+    function drawHexagon_f(canvasContext, col, row, fill) {
+        var fill = fill || false;
+        
+        col = col * (sideLength + hexHeight);
+        row = row * hexRectangleHeight + ((col % 2) * hexRadius);
+
+        canvasContext.beginPath();
+        canvasContext.moveTo(col + hexHeight, row);
+        canvasContext.lineTo(col + hexHeight + sideLength, row);
+        canvasContext.lineTo(col + hexRectangleWidth, row + hexRadius);
+        canvasContext.lineTo(col + hexHeight + sideLength, row + hexRectangleHeight);
+        canvasContext.lineTo(col + hexHeight, row + hexRectangleHeight);
+        canvasContext.lineTo(col, row + hexRadius);
+        canvasContext.closePath();
+
+        if (fill) {
+            canvasContext.fill();
+        } else {
+            canvasContext.stroke();
+        }
+    }
+
+    function offToCube_f(col, row) {
+        var x = col;
+        var z = row - (col - (col & 1)) / 2;
+        var y = -x - z;
+        return [x, y, z];
+    }
+
+    function cubeToAxial_f(x, y, z) {
+        var q = x;
+        var r = z;
+        return [q, r];
+    }
+
+    function offToAxial_f(col, row) {
+        var cube = offToCube_f(col, row);
+        var axial = cubeToAxial_f(cube[0], cube[1], cube[2]);
+        return [axial[0], axial[1]];
+    }
+
+    function axialToPixel_f(q, r) {
+        var x = sideLength * (3. / 2 * q);
+        var y = sideLength * (Math.sqrt(3) / 2 * q + Math.sqrt(3) * r);
+
+        //set pixel coordinates to the middle of the hexagon
+        x = Math.round(x + sideLength);
+        y = Math.round(y + hexRadius);
+        return [x, y];
+    }
+
+    function offToPixel_f(col, row) {
+        var axial = offToAxial_f(col, row);
+        var pixel = axialToPixel_f(axial[0], axial[1]);
+        return [pixel[0], pixel[1]];
+    }
+
+    function offNeighbor_f(col, row, direction) {
+        var parity = col & 1;
+        var dir = offDirections[parity][direction];
+        return [col + dir[0], row + dir[1]];
+    }
+    //=====
 })();
