0a5ce913d4a8b5c7bf57b4c5a40be1e96d2d5ae4
l3n4rt <62373525+l3n4rt@users.noreply.github.com>
2020-04-02 13:37:33 +0200

    added Models for Denizen, Job, Road, Species

---
+++ HexaGone/Models/Denizen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HexaGone.Models
+{
+    public class Denizen
+    {
+        //TODO: stats for denizen?
+
+        //Attributes
+        /// <summary>
+        /// The city where the denizen lives
+        /// </summary>
+        public City Hometown { get; set; }  
+
+        /// <summary>
+        /// The species of the denizen
+        /// </summary>
+        public Species DenizenSpecies { get; set; }
+
+        /// <summary>
+        /// The age of the denizen
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// The job of the denizen
+        /// </summary>
+        public Job DenizenJob { get; set; }
+    }
+}
