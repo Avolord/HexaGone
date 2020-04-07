9cfe63247ff1a2025e46818f6db0e272d9e4d636
fHostert <62373320+fHostert@users.noreply.github.com>
2020-04-07 12:25:20 +0200

    Units und so

---
+++ HexaGone/Models/Unit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HexaGone.Models
+{
+    public enum UnitClass
+    {
+        SwordAndShield,
+        SpearAndShield,
+        Pike,
+        Archer,
+        Cavalry,
+        Strike
+    }
+
+    public class Unit
+    {
+        //Attributes
+        /// <summary>
+        /// A unique ID for the Unit.
+        /// </summary>
+        public int UnitID { get; set; }
+        /// <summary>
+        /// A ID that specifies the class of the Unit.
+        /// </summary>
+        public UnitClass Class { get; set; }
+        /// <summary>
+        /// The name of the unit.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The Attack Stat of the unit.
+        /// </summary>
+        public float AttackStat { get; set; }
+        /// <summary>
+        /// The Defense Stat of the unit.
+        /// </summary>
+        public float DefenseStat { get; set; }
+        /// <summary>
+        /// The maximum number of men in the Unit.
+        /// </summary>
+        public int MaxMenCount { get; set; }
+        /// <summary>
+        /// The current number of men in the Unit.
+        /// </summary>
+        public int MenCount { get; set; }
+        /// <summary>
+        /// Is true if the Unit is a ranged Unit.
+        /// </summary>
+        public bool IsRangedUnit { get; set; }
+        /// <summary>
+        /// This Dictionary lists all multipliers this Unit gets against the other UnitClasses.
+        /// </summary>
+        public Dictionary<int, float> Factors { get; }
+        /// <summary>
+        /// Indicates how far the Unit can move in one turn.
+        /// </summary>
+        public float MovementPoints { get; set; }
+    }
+}
---
+++ HexaGone/Models/UnitRelations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HexaGone.Models
+{
+    static public class UnitRelations
+    {
+        static List<List<float>> AttackerBuffs { get; } = new List<List<float>>() {
+                                                          new List<float>() { 0, -0.05f, -0.15f, 0, -0.2f, -0.05f },
+                                                          new List<float>() { -0.05f, -0.05f, -0.15f, 0, 0.3f, -0.05f },
+                                                          new List<float>() { -0.1f, -0.15f, -0.25f, -0.2f, 0.7f, 0.05f },
+                                                          new List<float>() { -0.1f, -0.1f, 0.1f, 0.1f, 0.7f, 0.05f },
+
+                                                                                                                                };
+    }
+}
